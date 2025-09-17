2ac9e6f852bf4af30d305f5493d54dd705eeaec8
LorranDC <lorrandias150@gmail.com>
2025-09-16 21:20:51 -0300

    Exercício Pilha

---
+++ parentExe.py
@@ -0,0 +1,43 @@
+# Dado uma string de expressão x. 
+# Verifique se os pares e a ordem de ‘{’ , ‘}’ , ‘(’ , ‘)’ , ‘[’ , ‘]’ estão corretos.
+# Por exemplo, a função deve retornar:
+# ‘True’ para exp = “[()]{}{()()}” e 
+# ‘False’ para exp = “[(])”.
+
+from stack import Stack
+
+def is_balanced(expression):
+    """
+    Verifica se a expressão possui parênteses balanceados.
+    Usa a pilha implementada em stack.py.
+    """
+    pilha = Stack()
+
+    #------- COLOQUE SEU CÓDIGO AQUI -------
+    pares = {')': '(', '}': '{', ']': '['}
+    aberturas = "({["
+    
+    for char in expression: 
+        if char in aberturas:
+            pilha.push(char)
+        
+        elif char in pares:
+            if pilha.is_empty():
+                return False
+            
+            topo = pilha.pop()
+            if pares[char] != topo:
+                return False
+
+    
+    return pilha.is_empty()
+
+
+
+    #---------------------------------------
+
+
+# Teste
+print(is_balanced("[{}(2+2)]{}")) #Esperado True
+print(is_balanced("[{}(2+2))]{}")) #Esperado False
+print(is_balanced("[{}])")) #Esperado False
